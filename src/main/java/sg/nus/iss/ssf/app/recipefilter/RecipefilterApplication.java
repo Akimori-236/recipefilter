8500022582e26a737beae9cbbae0c56561ea5578
Wee Seng <wee.bscl@gmail.com>
2023-01-16 22:53:16 +0800

    init commit

---
+++ src/main/java/sg/nus/iss/ssf/app/recipefilter/RecipefilterApplication.java
@@ -0,0 +1,13 @@
+package sg.nus.iss.ssf.app.recipefilter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecipefilterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecipefilterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/sg/nus/iss/ssf/app/recipefilter/RecipefilterApplicationTests.java
@@ -0,0 +1,13 @@
+package sg.nus.iss.ssf.app.recipefilter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecipefilterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
